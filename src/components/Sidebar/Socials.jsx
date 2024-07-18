eee148e28a0b8820a20d1524ff57b98f8ab1f90d
Bryan Urrutia <66961781+Bryan-Urrutia@users.noreply.github.com>
2024-07-18 16:12:23 -0400

    Update Socials.jsx

--- src/components/Sidebar/Socials.jsx
+++ src/components/Sidebar/Socials.jsx
@@ -2,23 +2,22 @@
 
 import Link from 'next/link'
 import {
-    RiYoutubeFill,
     RiLinkedinFill,
     RiGithubFill,
+    RiYoutubeFill,
     RiFacebookBoxFill,
     RiInstagramFill
 } from 'react-icons/ri'
 
 const icons = [
-    { path: '/', name: <RiLinkedinFill /> },
-    { path: '/', name: <RiGithubFill /> },
-    { path: '/', name: <RiInstagramFill /> },
+    { path: 'https://www.linkedin.com/in/bryan-urrutia-36b57a257', name: <RiLinkedinFill /> },
+    { path: 'https://github.com/Bryan-Urrutia', name: <RiGithubFill /> },
 ]
 
 const Socials = ({ containerStyles, iconsStyles }) => {
     return <div className={`${containerStyles}`}>
         {icons.map((icon, index) => {
-            return <Link key={index} href={icon.path}>
+            return <Link key={index} href={icon.path} target='_blank'>
                 <div className={`${iconsStyles} p-2`}>{icon.name}</div>
             </Link>
         })}
