db14295ff492b9a617ce98d3a6558307540fb7f4
Bryan Urrutia <66961781+Bryan-Urrutia@users.noreply.github.com>
2025-01-06 00:20:18 -0300

    carta

--- src/hooks/useScrollProgress.jsx
+++ src/hooks/useScrollProgress.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const useScrollProgress = () => {
-    const [completion, setCompletion] = useState(0);
+  const [completion, setCompletion] = useState(0);
 
-    useEffect(() => {
-        const updateScrollCompletion = () => {
-            const currentProgress = window.scrollY;
-            const scrollHeight = document.body.scrollHeight - window.innerHeight;
+  useEffect(() => {
+    const updateScrollCompletion = () => {
+      const currentProgress = window.scrollY;
+      const scrollHeight = document.body.scrollHeight - window.innerHeight;
 
-            if (scrollHeight) {
-                setCompletion(Number(currentProgress / scrollHeight).toFixed(2) * 100);
-            }
-        }
-        window.addEventListener('scroll', updateScrollCompletion);
-        return () => window.removeEventListener('scroll', updateScrollCompletion);
-    }, []);
+      if (scrollHeight) {
+        setCompletion(Number(currentProgress / scrollHeight).toFixed(2) * 100);
+      }
+    };
+    window.addEventListener("scroll", updateScrollCompletion);
+    return () => window.removeEventListener("scroll", updateScrollCompletion);
+  }, []);
 
-    return completion;
-}
+  return completion;
+};
 
-export default useScrollProgress
+export default useScrollProgress;
